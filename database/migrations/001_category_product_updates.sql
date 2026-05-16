2a5aa4d4688c37a6bbb277b951715e22b4e33554
raseloriginal <raseloriginal369@gmail.com>
2026-05-16 15:40:38 +0600

    feat: make categories.company_id column nullable in database migration

--- database/migrations/001_category_product_updates.sql
+++ database/migrations/001_category_product_updates.sql
@@ -2,6 +2,3 @@
 
 -- 1. Make categories.company_id nullable
 ALTER TABLE categories MODIFY company_id INT NULL;
-
--- 2. Add box_type to products table
-ALTER TABLE products ADD COLUMN box_type VARCHAR(50) NULL AFTER name;
